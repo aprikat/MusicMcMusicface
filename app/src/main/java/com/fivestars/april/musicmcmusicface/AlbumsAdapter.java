3add153fa2570c552e29f29631b212bf419705af
aprikat <april.y.luo@gmail.com>
2016-05-26 18:38:05 -0700

    glide and loading images works

--- app/src/main/java/com/fivestars/april/musicmcmusicface/AlbumsAdapter.java
+++ app/src/main/java/com/fivestars/april/musicmcmusicface/AlbumsAdapter.java
@@ -1,11 +1,14 @@
 package com.fivestars.april.musicmcmusicface;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,6 +41,10 @@ public class AlbumsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         AlbumsResponse.Album album = albumList.get(position);
         AlbumHolder albumHolder= (AlbumHolder) holder;
 
+        Glide.with(holder.itemView.getContext())
+                .load(album.getImages().get(0).getUrl())
+                .fitCenter()
+                .into(albumHolder.ivAlbumImage);
         albumHolder.tvAlbumName.setText(album.getName());
     }
 
